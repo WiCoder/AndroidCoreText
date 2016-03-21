66c0c59442c56552b54c926c17f51187b078e410
yangzc <zhc_yang@163.com>
2016-03-21 15:37:33 +0800

    something test

--- library/src/main/java/com/hyena/coretext/ACTAttributedString.java
+++
@@ -1,8 +0,0 @@
-package com.hyena.coretext;
-
-/**
- * Created by yangzc on 16/3/19.
- */
-public class ACTAttributedString {
-
-}
--- library/src/main/java/com/hyena/coretext/node/Node.java
+++
@@ -1,7 +0,0 @@
-package com.hyena.coretext.node;
-
-/**
- * Created by yangzc on 16/3/19.
- */
-public class Node {
-}
